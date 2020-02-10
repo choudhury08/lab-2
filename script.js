8da6f254dde3867447263fb00d5f62d39f2d4b06
Mahbubur Choudhury <mahbubc2012@gmail.com>
2020-02-10 17:32:41 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function longest_name(arr) {
+  for (let i = 0, j = 1; i, j < arr.length; i++, j++)
+  arr.push(prompt("Enter a number");
+    if (arr[i].length < arr[j].length) console.log(arr[j]);
+}
+longest_name(["John", "Tom", "Benjamin"]);
